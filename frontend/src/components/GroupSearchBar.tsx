de8fbad3135b9e0cbf056360823684c249f126ae
Carter Broom <146979015+carterdbroom@users.noreply.github.com>
2025-01-11 19:21:34 -0500

    I LOVE WEB DEVELOPMENT!!!

---
+++ frontend/src/components/GroupSearchBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function GroupSearchBar() {
+    return (
+        <form> 
+            <input type="text" placeholder="Search for a group" />
+        </form>
+    );
+}
