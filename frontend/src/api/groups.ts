89e0e8eef6cf9ea2deb23614f7dc115069b60d75
Austin Bray <93416177+AustinBray77@users.noreply.github.com>
2025-01-11 21:52:10 -0500

    Fixed api integration functions

--- frontend/src/api/groups.ts
+++ frontend/src/api/groups.ts
@@ -1,26 +1,46 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { Group } from "../types/group";
 
-function getAllGroups(): Promise<Group> {
-    return axios.post("/api/groups/all", JSON.stringify({}), {
-        headers: { "Content-Type": "application/json" },
-    });
+function getAllGroups(): Promise<Group[]> {
+    return axios
+        .post("/api/groups/all", JSON.stringify({}), {
+            headers: { "Content-Type": "application/json" },
+        })
+        .then((res: AxiosResponse<any, any>) => {
+            return res.data["groups"] as Group[];
+        })
+        .catch((err) => {
+            console.error(err);
+            return [] as Group[];
+        });
 }
 
-function getGroupsByFilters(filters: any): Promise<Group> {
-    return axios.post("/api/groups/all", JSON.stringify({ filters: filters }), {
-        headers: { "Content-Type": "application/json" },
-    });
+function getGroupsByFilters(filters: any): Promise<Group[]> {
+    return axios
+        .post("/api/groups/all", JSON.stringify({ filters: filters }), {
+            headers: { "Content-Type": "application/json" },
+        })
+        .then((res: AxiosResponse<any, any>) => {
+            return res.data["groups"] as Group[];
+        })
+        .catch((err) => {
+            console.error(err);
+            return [] as Group[];
+        });
 }
 
-function getGroupsFromUser(user_id: string): Promise<Group> {
-    return axios.post(
-        "/api/groups/from-user",
-        JSON.stringify({ user_id: user_id }),
-        {
+function getGroupsFromUser(user_id: string): Promise<Group[]> {
+    return axios
+        .post("/api/groups/from-user", JSON.stringify({ user_id: user_id }), {
             headers: { "Cntent-Type": "application/json" },
-        }
-    );
+        })
+        .then((res: AxiosResponse<any, any>) => {
+            return res.data["groups"] as Group[];
+        })
+        .catch((err) => {
+            console.error(err);
+            return [] as Group[];
+        });
 }
 
 export { getAllGroups, getGroupsByFilters, getGroupsFromUser };
--- frontend/src/api/streaks.ts
+++ frontend/src/api/streaks.ts
@@ -1,20 +1,34 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { Streak } from "../types/streak";
 
 function getUserStreaks(user_id: string): Promise<Streak> {
-    return axios.post(
-        "/api/streaks/get",
-        JSON.stringify({ user_id: user_id }),
-        { headers: { "Content-Type": "application/json" } }
-    );
+    return axios
+        .post("/api/streaks/get", JSON.stringify({ user_id: user_id }), {
+            headers: { "Content-Type": "application/json" },
+        })
+        .then((res: AxiosResponse<any, any>) => {
+            return res.data["streaks"] as Streak;
+        })
+        .catch((err) => {
+            console.error(err);
+            return {} as Streak;
+        });
 }
 
-function incrementStreak(user_id: string, streak: string): Promise<Streak> {
-    return axios.post(
-        "/api/streaks/increment",
-        JSON.stringify({ user_id: user_id, streak: streak }),
-        { headers: { "Content-Type": "application/json" } }
-    );
+function incrementStreak(user_id: string, streak: string): Promise<boolean> {
+    return axios
+        .post(
+            "/api/streaks/increment",
+            JSON.stringify({ user_id: user_id, streak: streak }),
+            { headers: { "Content-Type": "application/json" } }
+        )
+        .then((res: AxiosResponse<any, any>) => {
+            return true;
+        })
+        .catch((err) => {
+            console.error(err);
+            return false;
+        });
 }
 
 export { getUserStreaks, incrementStreak };
