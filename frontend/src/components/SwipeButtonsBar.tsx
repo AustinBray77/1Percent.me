4b4c8aff346d7d53cac9b1d05af7f7ae2abacf39
Austin Bray <93416177+AustinBray77@users.noreply.github.com>
2025-01-12 09:35:33 -0500

    Added group card functionality

--- frontend/src/components/SwipeButtonsBar.tsx
+++ frontend/src/components/SwipeButtonsBar.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 import { FaHeartBroken } from "react-icons/fa";
-import LikeButton from './LikeButton.tsx';
-import DislikeButton from './DislikeButton.tsx';
+import LikeButton from "./LikeButton.tsx";
+import DislikeButton from "./DislikeButton.tsx";
 
-export default function SwipeButtonsBar() {
+export default function SwipeButtonsBar(props: {
+    onLike: () => void;
+    onDislike: () => void;
+}) {
     return (
         <div className="buttonBar">
-            <DislikeButton />
-            <LikeButton />
+            <DislikeButton onClick={props.onDislike} />
+            <LikeButton onClick={props.onLike} />
         </div>
     );
 }
